1ac80ca86ccdff2c62093d57b54bdd83f3016274
qianzsh <simonw713jb@gmail.com>
2025-03-08 19:56:19 +0800

    Add boundary check in q_remove_tail
    - Add a NULL and empty queue check to prevent undefined behavior.
    
    Change-Id: I94a5d7348e8100d7b79ca29a68259d805bdc7e3e

--- queue.c
+++ queue.c
@@ -111,6 +111,9 @@ element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
+    if (!head || list_empty(head))
+        return NULL;
+
     element_t *lastnode = list_entry(head->prev, element_t, list);
 
     head->prev = lastnode->list.prev;
