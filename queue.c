8d1027d7483aacb6e531fd53426672962b6d3b86
qianzsh <simonw713jb@gmail.com>
2025-03-09 11:09:32 +0800

    Add check and unlink element in q_remove_tail
    - Ensure "bufsize > 0" before copying the string.
    - Use "list_del" to unlink the element from the list without freeing its
      memory.
    
    Change-Id: I0aa560aae8b95216c4ccd3c572dc6d721230877f

--- queue.c
+++ queue.c
@@ -114,10 +114,8 @@ element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 
     element_t *lastnode = list_last_entry(head, element_t, list);
 
-    head->prev = lastnode->list.prev;
-    lastnode->list.prev->next = head;
-
-    if (sp)
+    list_del(&lastnode->list);
+    if (sp && bufsize > 0)
         strlcpy(sp, lastnode->value, bufsize);
 
     return lastnode;
